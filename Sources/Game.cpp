6a4e4559988543f2e74d8ebe7dbbf1c58dc2dd7e
HW15A213 Yamamoto.Yuki <hw15a213@oecu.jp>
2017-11-21 16:22:25 +0900

    Fの作業の実装の完了

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -6,7 +6,7 @@
 // TODO: 砲台を青い壁に沿って上下に動かす。(C)
 // TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
 // TODO: スコアのサイズを大きくする。(E)//HW15A213 山本 裕生
-// TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
+// TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)//HW15A213 山本 裕生
 // TODO: PlayBGM()関数を使って、BGMを再生する。(G)
 // TODO: PlaySE()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H)
 
@@ -43,7 +43,7 @@ void Update()
         // ターゲットと弾の当たり判定
         Rect bulletRect(bulletPos, Vector2(32, 20));
         if (targetRect.Overlaps(bulletRect)) {
-            score += 1;         // スコアの加算
+            score += 100;         // スコアの加算 //HW15A213 山本 裕生
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
         }
     }
@@ -68,8 +68,8 @@ void Update()
     FillRect(targetRect, Color::red);
 
     // スコアの描画
-    SetFont("nicoca_v1.ttf", 70.0f);
-    DrawText(FormatString("%02d", score), Vector2(-319, 180), Color::black);//HW15A213 山本 裕生
-    DrawText(FormatString("%02d", score), Vector2(-320, 181), Color::white);//HW15A213 山本 裕生
+    SetFont("nicoca_v1.ttf", 70.0f);//HW15A213 山本 裕生
+    DrawText(FormatString("%05d", score), Vector2(-319, 180), Color::black);//HW15A213 山本 裕生
+    DrawText(FormatString("%05d", score), Vector2(-320, 181), Color::white);//HW15A213 山本 裕生
 }
 
